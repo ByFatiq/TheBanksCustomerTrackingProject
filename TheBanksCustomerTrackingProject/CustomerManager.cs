566ba1bc027a4ff6e898c3cf671019bb52fd423d
Fatih <fatihgs1433@gmail.com>
2022-02-18 00:26:00 +0300

    Add project files.

---
+++ TheBanksCustomerTrackingProject/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBanksCustomerTrackingProject
+{
+    class CustomerManager
+    {
+         public void Add(Customer customer)
+        {
+
+            Console.WriteLine("Sayın; "+ customer.FirstName + " "+ customer.LastName+" Bankamıza Hoşgeldiniz.");
+            
+        }
+
+
+         public void Delete(Customer customer)
+         {
+
+             Console.WriteLine(customer.FirstName + " " + customer.LastName +" Tekrar görüşmek dileğiyle..");
+
+         }
+
+         public void List(Customer[] customers)
+         {
+             foreach (var customer in customers)
+             {
+                 Console.WriteLine(customer.FirstName + " " + customer.LastName);
+             }
+         }
+    }
+}
---
+++ TheBanksCustomerTrackingProject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace TheBanksCustomerTrackingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+
+            customer1.Id = 1;
+            customer1.FirstName = "Fatih";
+            customer1.LastName = "C.";
+            customer1.Gender = "E";
+            customer1.IdentityNumber = "123456";
+            customer1.Age = 26;
+
+            Customer customer2 = new Customer();
+
+            customer2.Id = 1;
+            customer2.FirstName = "Fatih";
+            customer2.LastName = "Ceker";
+            customer2.Gender = "E";
+            customer2.IdentityNumber = "123456";
+            customer2.Age = 26;
+
+            Customer[] customers = new Customer[]{ customer1, customer2 };
+            CustomerManager cm = new CustomerManager();
+
+            cm.Add(customer1);
+            cm.Add(customer2);
+
+
+            Console.WriteLine(" <====Müşteriler Eklendi.======>\n\n");
+
+
+            cm.List(customers);
+
+            Console.WriteLine(" <====Müşteriler Listelendi.======>\n\n");
+
+
+
+
+
+
+            cm.Delete(customer1);
+            cm.Delete(customer2);
+
+
+            Console.WriteLine(" <====Müşteriler Silindi.======>");
+
+
+
+
+
+           
+
+         
+        }
+    }
+}
